e0a44748356d865463d2303cc14f7af1a213c12d
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-06-28 20:36:32 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -39,9 +39,20 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Erro para reservar, as datas tem que ser futuras";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Erro para reservar, o check out e antes do que o check in";
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
